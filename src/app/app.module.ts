11513817453e33928602337c498c9e6cff5942cf
Rahul Devdurg <rahuldevdurg14@gmail.com>
2021-02-04 16:08:11 +0530

    chnages in department select option

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 
 import { EmployeesComponent } from './employees/employees.component';
 import { FormsModule } from '@angular/forms';
-import { Ng2SearchPipeModule } from 'ng2-search-filter'
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { HomeComponent } from './home/home.component';
+import { AddnewComponent } from './addnew/addnew.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    HomeComponent,
+    AddnewComponent
   ],
   imports: [
     BrowserModule,
--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { IEmployees } from '../model/employee.interface';
 
 
 @Component({
@@ -12,64 +11,89 @@ export class EmployeesComponent implements OnInit {
 
   constructor() {  };
     searchText: string="";
-    sortDir = 1;//1= 'ASE' -1= DSC
-  
+    employee:any[]=[];
+    department:string[]=[];
     
+   
+  
+  ngOnInit(): void {
+    this.employee = [
 
-      employee : IEmployees [] = [
+          { 
+           name: "Employee One",
+           age: 40,
+           email: "one@gmail.com",
+           department: ["Computer", "Physics"],
+          },
+       
+  
+          { 
+           name: "Employee Two",
+           age: 10,
+           email: "Two@gmail.com",
+           department: ["Computer"],
+          },
+       
+          {
+           name: "Employee Three",
+           age: 10,
+           email: "Three@gmail.com",
+           department: ["Physics", "Chemistry"],
+         },
+  
+         {
+           name: "Employee Four",
+           age: 60,
+           email: "Four@gmail.com",
+           department: ["Chemistry", "Physics"],
+         },
+       
+         {
+           name: "Employee Five",
+           age: 70,
+           email: "Five@gmail.com",
+           department: ["Computer", "Physics", "Chemistry"],
+         },
+       
+         {
+           name: "Employee Six",
+           age: 70,
+           email: "Six@gmail.com",
+           department: ["Computer", "Physics"],
+         },
+       
+         { 
+           name: "Employee Seven",
+           age: 10,
+           email: "Seven@gmail.com",
+           department: ["Computer"],
+          },
+          { 
+            name: "Employee Seven",
+            age: 10,
+            email: "Seven@gmail.com",
+            department: ["Electronics"],
+           },
+         ];
+       
 
-        { 
-         name: "Employee One",
-         age: 40,
-         email: "one@gmail.com",
-         department: ["Computer", "Physics"],
-        },
+         let deps = this.employee.map((x) => x.department);
+          //console.log('All Deps - ', deps);
      
-        { 
-         name: "Employee Two",
-         age: 10,
-         email: "Two@gmail.com",
-         department: ["Computer"],
-        },
-     
-        {
-         name: "Employee Three",
-         age: 10,
-         email: "Three@gmail.com",
-         department: ["Physics", "Chemistry"],
-       },
+         for (const da of deps) {
+           for (const d of da) {
+             if (this.department.indexOf(d) === -1) {
+              // console.log('Unique Dep - ', d);
+               this.department.push(d);
+             }
+           }
+         }
 
-       {
-         name: "Employee Four",
-         age: 60,
-         email: "Four@gmail.com",
-         department: ["Chemistry", "Physics"],
-       },
-     
-       {
-         name: "Employee Five",
-         age: 70,
-         email: "Five@gmail.com",
-         department: ["Computer", "Physics", "Chemistry"],
-       },
-     
-       {
-         name: "Employee Six",
-         age: 70,
-         email: "Six@gmail.com",
-         department: ["Computer", "Physics"],
-       },
-     
-       { 
-         name: "Employee Seven",
-         age: 10,
-         email: "Seven@gmail.com",
-         department: ["Computer"],
-        },
-       ];
-     
-       ngOnInit(): void {
+         
+        
        }
+
+    
 
        refresh(): void {
         window.location.reload();
